897770658d9ddb3b0d9a04917ada118c6769ebeb
Jose <jrosales0001@gmail.com>
2018-01-29 10:39:27 -0500

    date sleep

--- ExtraLab/ChooseAdventure.cs
+++ ExtraLab/ChooseAdventure.cs
@@ -13,28 +13,33 @@ namespace ExtraLab
         static bool RunProgram = true;
         static void Main(string[] args)
         {
-
             Console.Title = "Choose Your Own Adventure: Forest Breeze"; //TODO: Outer space, Cave, Mountains, Ocean, City
-            while (RunProgram)
-            {
-                string Name = ReadUserInput("What's your name?");
-                Console.Clear();
-                while (ChooseSkills)
-                {
-                    string FirstSkill = FirstSkills($"Pick a skill {Name}!\n[1] Archery\t[2] Baking");
-                    Console.Clear();
-                    string SecondSkill = SecondSkills("It's pretty dangerous out there. Pick another skill.\n" +
-                                                      "[3] Making jewlery\t[4] Poetry");
-                    Console.Clear();
-                    Console.WriteLine("You chose {0} and {1}!\nAlright, {2} do you want to continue " +
-                                      "or choose different skills?", FirstSkill, SecondSkill, Name);
 
-                    Console.WriteLine("\n[Y] Yes, continue.\t[N] Wait, I want to change my skills.");
-                    bool UserDecision = Decision(char.Parse(Console.ReadLine()));
-                }
-                Console.Clear();                
-                Console.WriteLine("It was a perfectly normal //current date.time");
+            string Name = ReadUserInput("What's your name?");
+            Console.Clear();
+            while (ChooseSkills)
+            {
+                string FirstSkill = FirstSkills($"Pick a skill {Name}!\n[1] Archery\t[2] Baking");
+                Console.Clear();
+                string SecondSkill = SecondSkills("It's pretty dangerous out there. Pick another skill.\n" +
+                                                  "[3] Making jewlery\t[4] Poetry");
+                Console.Clear();
+                Console.WriteLine("You chose {0} and {1}!\nAlright, {2} do you want to continue " +
+                                  "or choose different skills?", FirstSkill, SecondSkill, Name);
+
+                Console.WriteLine("\n[Y] Yes, continue.\t[N] Wait, I want to change my skills.");
+                bool UserDecision = Decision(char.Parse(Console.ReadLine()));
             }
+            Console.Clear();
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+            System.Threading.Thread.Sleep(1000);
+
+            Console.WriteLine("\nIt was a perfectly normal day...");
+            
+
         }
         public static string ReadUserInput(string UserPrompt)
         {
