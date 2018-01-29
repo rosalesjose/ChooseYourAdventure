4f466fe0f552630dbd6c339245a78ac00270c4ed
Jose <jrosales0001@gmail.com>
2018-01-28 22:15:46 -0500

    finished intro

--- ExtraLab/ChooseAdventure.cs
+++ ExtraLab/ChooseAdventure.cs
@@ -9,23 +9,32 @@ namespace ExtraLab
 {
     class Program
     {
-        static bool RunApplication = true;
-
+        static bool ChooseSkills = true;
+        static bool RunProgram = true;
         static void Main(string[] args)
-        {                 
+        {
+
             Console.Title = "Choose Your Own Adventure: Forest Breeze"; //TODO: Outer space, Cave, Mountains, Ocean, City
-               
-            string Name = ReadUserInput("What's your name?");
-            Console.Clear();
-            string FirstSkill = FirstSkills($"Pick a skill {Name}!\n[1] Archery\t[2] Baking");
-            Console.Clear();
-            string SecondSkill = SecondSkills("It's pretty dangerous out there. Pick another skill.\n" +
-                                              "[3] Making jewlery\t[4] Poetry");
-            Console.Clear();
-            Console.WriteLine("You chose {0} and {1}!\nAlright, {2} do you want to continue " +
-                              "or choose different skills?", FirstSkill, SecondSkill, Name);
-            Console.WriteLine("[Y] Yes, continue.\t[N] Wait, I want to change my skills.");
-            
+            while (RunProgram)
+            {
+                string Name = ReadUserInput("What's your name?");
+                Console.Clear();
+                while (ChooseSkills)
+                {
+                    string FirstSkill = FirstSkills($"Pick a skill {Name}!\n[1] Archery\t[2] Baking");
+                    Console.Clear();
+                    string SecondSkill = SecondSkills("It's pretty dangerous out there. Pick another skill.\n" +
+                                                      "[3] Making jewlery\t[4] Poetry");
+                    Console.Clear();
+                    Console.WriteLine("You chose {0} and {1}!\nAlright, {2} do you want to continue " +
+                                      "or choose different skills?", FirstSkill, SecondSkill, Name);
+
+                    Console.WriteLine("\n[Y] Yes, continue.\t[N] Wait, I want to change my skills.");
+                    bool UserDecision = Decision(char.Parse(Console.ReadLine()));
+                }
+                Console.Clear();                
+                Console.WriteLine("It was a perfectly normal //current date.time");
+            }
         }
         public static string ReadUserInput(string UserPrompt)
         {
@@ -35,18 +44,18 @@ namespace ExtraLab
             if (InputRegex.IsMatch(Name))
             {
                 return Name;
-            }                      
+            }
             return ReadUserInput(UserPrompt);
-        }   
+        }
 
-        public static string FirstSkills (string SkillChoice1)
+        public static string FirstSkills(string SkillChoice1)
         {
             Console.WriteLine(SkillChoice1);
             string Choice = Console.ReadLine().Trim();
 
             var Archery = new Regex(@"^[1]$");
             var Baking = new Regex(@"^[2]$");
-            
+
 
             if (Archery.IsMatch(Choice))
             {
@@ -64,7 +73,7 @@ namespace ExtraLab
             return FirstSkills(SkillChoice1);
         }
 
-        public static string SecondSkills (string SkillChoice2)
+        public static string SecondSkills(string SkillChoice2)
         {
             Console.WriteLine(SkillChoice2);
             string Choice = Console.ReadLine().Trim();
@@ -86,6 +95,20 @@ namespace ExtraLab
             }
             Console.Clear();
             return SecondSkills(SkillChoice2);
-        }       
+        }
+
+        public static bool Decision(char UserDecision)
+        {
+            if (UserDecision == 'n' || UserDecision == 'N')
+            {
+                Console.Clear();
+                return ChooseSkills = true;
+            }
+            else if (UserDecision == 'y' || UserDecision == 'Y')
+            {
+                return ChooseSkills = false;
+            }
+            return Decision(UserDecision);
+        }
     }
 }
